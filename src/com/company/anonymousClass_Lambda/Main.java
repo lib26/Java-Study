d8555c784e4f3c01603f78c6223b8d52f3eccdb0
lib26 <dladlsqja@naver.com>
2023-01-17 20:43:13 +0900

    lambda

--- src/com/company/anonymousClass_Lambda/Main.java
+++ src/com/company/anonymousClass_Lambda/Main.java
@@ -6,28 +6,22 @@ public class Main {
         int num1 = 20;
         int num2 = 10;
 
-        // Calculator 클래스 생성하며 계산 할 수를 클래스 필드에 저장
         Calculator calculator = new Calculator(num1, num2);
 
-        // calculator.caculate() 메서드 인자로, operate() 추상 메소드를 더하기 연산이 되도록 재정의한 익명 구현 객체를 넘김
+        // 재정의한 익명 구현 객체를 caculate() 인자로
         // calculator.caculate() 매서드 내에서 재정의된 operate() 메소드가 실행되어 a + b가 리턴 됨
         int result = calculator.caculate(new Operate() {
             public int operate(int a, int b) {
                 return a + b;
             }
         });
-
         System.out.println(result); // 30
 
-
-        // calculator.caculate() 메서드 인자로, operate() 추상 메소드를 빼기 연산이 되도록 재정의한 익명 구현 객체를 넘김
-        // calculator.caculate() 매서드 내에서 재정의된 operate() 메소드가 실행되어 a - b가 리턴 됨
-        int result2 = calculator.caculate(new Operate() {
-            public int operate(int a, int b) {
-                return a - b;
-            }
-        });
-
+        // operate() 재정의한 익명 구현 객체를 람다 표현식으로 바꿈
+        // calculator.caculate() 매서드에 람다식을 넣음
+        Operate operate = (a, b) -> a - b;
+        int result2 = calculator.caculate(operate);
         System.out.println(result2); // 10
+
     }
 }
